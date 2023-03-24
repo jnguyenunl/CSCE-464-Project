814016363ab32dd981440c28a9f3a71e379241ef
James Nguyen <jn31346240@gmail.com>
2023-03-24 00:17:21 -0500

    Implement dark mode

---
+++ dark.js
@@ -0,0 +1,6 @@
+function darkMode(){
+    var body = document.body;
+    body.classList.toggle("dark-mode");
+    var header = document.getElementById("header");
+    header.classList.toggle("dark-mode-header");
+}
